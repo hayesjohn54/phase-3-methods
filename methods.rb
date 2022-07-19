602a71a867fa89e0d50d4df805a34a54adb5d722
John <hayesjohn54@yahoo.com>
2022-07-19 14:11:57 -0400

    commit tree 76503086f633eab86f76a1dacbcc843a43948b69

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name = "John")
+    puts "Hello, #{name}!"
+end
+
+greet
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+def add(num1 = 1, num2 = 2)
+    num1 + num2
+end
+
+add
